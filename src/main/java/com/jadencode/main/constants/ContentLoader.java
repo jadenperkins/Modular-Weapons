635f329970aa14a2f5c7b68d893e52dc60b6ef53
JadenPerkins <jperkins@rsmc.io>
2016-06-15 15:57:40 -0400

    Added ContentManager with method load() to be populated by calls to static classes to externalize customizable content

---
+++ src/main/java/com/jadencode/main/constants/ContentLoader.java
@@ -0,0 +1,18 @@
+package com.jadencode.main.constants;
+
+/**
+ * Created by gtrpl on 6/15/2016.
+ */
+public final class ContentLoader {
+    public static final void load() {
+        //Colors
+        //Stats
+        //MaterialTypes
+        //PartTypes
+        //StatSets
+        //MaterialModifiers
+        //Materials
+        //WeaponTypes
+        //WeaponParts
+    }
+}
