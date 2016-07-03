eb908fd7e136d26a974f583271433985254d7060
JadenPerkins <jperkins@rsmc.io>
2016-07-03 14:25:39 -0400

    Idk why this wasn't committed

---
+++ src/main/java/com/jadencode/main/renderengine/RawModel.java
@@ -0,0 +1,21 @@
+package com.jadencode.main.renderengine;
+
+/**
+ * Created by gtrpl on 7/2/2016.
+ */
+public class RawModel {
+    private final int vaoID;
+    private final int vertexCount;
+
+    public RawModel(int i, int j) {
+        this.vaoID = i;
+        this.vertexCount = j;
+    }
+    public int getVaoID() {
+        return vaoID;
+    }
+    public int getVertexCount() {
+        return vertexCount;
+    }
+
+}
