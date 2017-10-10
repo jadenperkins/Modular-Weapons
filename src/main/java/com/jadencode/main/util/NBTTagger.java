5122ba1ae2564587df133a510cb5fe585466f62c
Jaden <jadenperkins@gmail.com>
2017-10-09 21:29:24 -0400

    Clean-up for new project

--- src/main/java/com/jadencode/main/util/NBTTagger.java
+++
@@ -1,9 +0,0 @@
-package com.jadencode.main.util;
-
-import com.jadencode.main.nbt.NBTTagCompound;
-
-public interface NBTTagger<T>
-{
-	public NBTTagCompound toNBT(T object);
-	public T fromNBT(NBTTagCompound nbt);
-}
--- src/main/kotlin/example/Run.kt
+++
@@ -1,9 +0,0 @@
-package example
-
-/**
- * Created by Andy on 6/5/2016.
- */
-fun main(args : Array<String>) {
-    val receiver = LowerReceiver()
-    println(receiver)
-}
